71e85a96d9c2114cc6af4fff69ac2e739e5dd284
Nathan Good <nathgood@amazon.com>
2016-03-14 09:43:41 -0500

    Refactoring

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 # This file contains sensible defaults
 
 # Assume that the user will want git and java
-default['jenkins_master']['plugin_profiles'] = ['git', 'java']
+default['jenkins_master']['plugin_profiles'] = ['git', 'java', 'aws']
--- recipes/users.rb
+++
@@ -1,7 +0,0 @@
-# This file will create a Jenkins admin user so that the Jenkins master server is not
-# accessible by just anyone.
-jenkins_password_credentials 'jenkins' do
-  id '04563300-2aa9-467c-a341-4180ca7af1c6'
-  description 'Jenkins Admin'
-  password 'jenkinsadmin'
-end
